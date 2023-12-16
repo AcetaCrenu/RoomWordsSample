efb1c205d716dc0b2871eb0f909aed763876f789
MerbahHouda <houdamerbah111@gmail.com>
2023-12-16 11:38:53 +0100

    1st commit

---
+++ app/src/main/java/ma/ensaj/roomwordssample/model/Word.java
@@ -0,0 +1,28 @@
+package ma.ensaj.roomwordssample.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName="Word_table")
+public class Word {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @NonNull
+    @ColumnInfo(name="word")
+    private String mWord;
+    public Word(@NonNull String word){
+        this.mWord=word;
+    }
+    public String getWord(){
+        return this.mWord;}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
